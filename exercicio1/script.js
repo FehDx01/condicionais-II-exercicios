8ee1b8eee7583fbdf721600523a3c77faae69acd
FehDx01 <lessa.fernando01@gmail.com>
2023-11-30 18:44:07 -0300

    exercícios da aula de condicionais II

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+/* const numeroDivisivel = +prompt("informe um número")
+
+ if (numeroDivisivel % 2 === 0) {
+    if (numeroDivisivel % 3 === 0) {
+        console.log("O número é divisivel por 2 e por 3");
+    } else {
+        console.log("informe um número divisivel 2")
+    }
+
+} else {
+    console.log("informe um numero divisivel por 2 ou 3");
+} 
+
+//PARTE 01 OPERADOR LÓGICO
+ if (numeroDivisivel % 2 === 0 && numeroDivisivel % 3 === 0) {
+    console.log("O número é divisivel por 2 e por 3");
+} else {
+    console.log("informe um número divisivel por 2 ou 3");
+}
+
+//PARTE 02 OPERADOR LÓGICO
+if (numeroDivisivel % 2 === 0 || numeroDivisivel % 3 === 0) {
+    console.log("O número é divisivel por 2 e por 3");
+} else {
+    console.log("informe um número divisivel por 2 ou 3");
+} */
